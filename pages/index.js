0be464c68319724e73502f720d20503340e2ace9
Eduardo Tortato de Araujo Bastos <etortato@gmail.com>
2020-12-14 23:42:45 -0300

    Adicionando imagens

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
+import Background from "./backgroud";
+
 function Home() {
-    return <div>Esse será o novo site da Atualize Construtora - Teste</div>
+  return (
+    <Background>
+      <div>Esse será o novo site da Atualize Construtora - Teste</div>
+    </Background>
+  );
 }
 
-export default Home
+export default Home;
