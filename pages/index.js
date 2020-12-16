0bcda526a220e0ab1f1730e54fed7b02c22572d0
Eduardo Tortato de Araujo Bastos <etortato@gmail.com>
2020-12-16 00:02:24 -0300

    Novo carrossel

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,22 @@
-import Background from "./backgroud";
+import Image from "next/image";
+import Carousel from "re-carousel";
 
 function Home() {
   return (
-    <Background>
-      <div>Esse será o novo site da Atualize Construtora - Teste</div>
-    </Background>
+    <div style={{ height: "100vh" }}>
+      <style jsx global>{`
+        body {
+            margin: 0px;
+        }
+      `}</style>
+      <Carousel loop auto interval="7000">
+        <Image key="1" src="/1.jpg" layout="fill" />
+        <Image key="2" src="/2.jpg" layout="fill" />
+        <Image key="3" src="/3.jpg" layout="fill" />
+        <Image key="4" src="/4.jpg" layout="fill" />
+        <Image key="5" src="/5.jpg" layout="fill" />
+      </Carousel>
+    </div>
   );
 }
 
