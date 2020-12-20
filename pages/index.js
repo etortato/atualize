702d68ed436bd88555efc186582f1eb005953f1f
Eduardo Tortato de Araujo Bastos <etortato@gmail.com>
2020-12-20 00:07:43 -0300

    Quase lá

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,29 @@
 import Image from "next/image";
 import Carousel from "re-carousel";
 
+function Texto(props) {
+  return (
+    <div
+      style={{
+        left: "50%",
+        background: "rgba(255, 255, 255, 0.85)",
+        zIndex: 2000,
+        position: "absolute",
+        bottom: "10%",
+        transform: "translate(-50%)",
+        padding: 20,
+        borderRadius: 8,
+        fontFamily: "sans-serif",
+        fontSize: "x-large",
+        fontWeight: "bold",
+        width: "70%"
+      }}
+    >
+      {props.text}
+    </div>
+  );
+}
+
 function Menu(props) {
   return (
     <div
@@ -56,18 +79,7 @@ function Home() {
         }
       `}</style>
       <Header></Header>
-      <div
-        style={{
-          top: "50%",
-          left: "50%",
-          background: "rgba(0,0,0,0.2)",
-          position: "absolute",
-          zIndex: 2000,
-          color: "white",
-        }}
-      >
-        ALGUM TEXTO BEM MANEIRO AQUI
-      </div>
+      <Texto text="Prestadora de serviços que atua na área da construção civil, reformas e manutenção. Uma empresa que do início de sua história até hoje viveu inúmeros desafios e aprendizados. Experiências que nos ensinaram a construir sonhos firmados em princípios de confiança, transparência, respeito, qualidade, eficiência, segurança e inovação." />
       <Carousel loop auto interval="7000">
         <Image key="1" src="/1.jpg" layout="fill" />
         <Image key="2" src="/2.jpg" layout="fill" />
